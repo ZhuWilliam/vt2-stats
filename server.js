394228e66c36db82566333b2421fc9ad4d25ef39
William Zhu <wbzhu@ucsd.edu>
2018-11-09 13:24:40 -0800

    Add working POST functionality

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.static('vt2-stats'));
 
 // Database
-const sqlite3 = require('sqlite3');
+const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('vt2.db');
 
 app.listen(3000, () => {
@@ -39,98 +39,66 @@ app.get('/average/:title/:artist', (req, res) => {
   );
 });
 
+function checkIfNameExists(name) {
+  var stmt = db.prepare('SELECT COUNT(*) AS "count" FROM player WHERE name=?');
+  var tst = stmt.get(name);
+  console.log(tst);
+  
+  stmt.finalize();
+}
+
 // POST requests
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 /* Add a game to the database. */
 app.post('/addGame/', function(req, res) {
-  db.get('SELECT COUNT(*) AS "count" FROM game', function(err, row) {
-    const curGameID = row.count + 1;
-    
-    db.run(
-      'INSERT INTO game VALUES ($curGameID, $difficulty, $map, $didWin)',
-      {
-        $curGameID: curGameID,
-        $difficulty: req.body.difficulty,
-        $map: req.body.map,
-        $didWin: req.body.didWin
-      },
-      function(err) {
-        if (err) {
-          console.log('Error in app.post(/addGame/) on INSERTING game: ' + err);
-        }
-      }
-    );
-    
-    // Inserting all player info into (player) and (played_in)
-    for (var i = 0; i < 4; ++i) {
-      console.log("------ i: " + i);
+  db.serialize(function() {
+    db.get('SELECT COUNT(*) AS "count" FROM game', function(err, row) {
+      const curGameID = row.count + 1;
+      var stmt = db.prepare('INSERT INTO game VALUES (?, ?, ?, ?)')
+      stmt.run(curGameID, req.body.difficulty, req.body.map, req.body.didWin);
+      stmt.finalize();
       
-      /*db.get(
-        'SELECT COUNT(*) AS "count" FROM player WHERE name = $name',
-        {
-          $name: req.body.players[i].name
-        },
-        function(err, row) {
-          // if the player is not in the player table yet, add them
-          console.log("DID WE MAKE IT< CAPTAIN");
-          console.log(row);
-          if (row.cnt === 0) {
-            console.log("--------------- i: " + i);
-            */
-            db.run(
-              'INSERT INTO player VALUES ($name, NULL)',
-              {
-                $name: req.body.players[i].name
-              },
-              function(err) {
-                if (err) {
-                  console.log('Error in app.post(/addGame) on INSERTING player: ' + err);
-                  console.log('attempted to insert: ' +  req.body.players[i].name)
-                }
-                else {
-                  console.log("INSERT TO player SUCCESS")
-                }
-              }
-            );
-          /*}
-        }
-      );*/
+      // Inserting all player info into (player) and (played_in)
+      for (var i = 0, k = 0; i < 4; ++i) {
+        db.serialize(function() {
+          checkIfNameExists(req.body.players[k].name);
+          stmt = db.prepare('SELECT * FROM player WHERE name=?');
+          stmt.get(req.body.players[k].name, function(err, row) {
+            // if the player is not in the player table yet, add them
+            if (!row) {
+              stmt = db.prepare('INSERT INTO player VALUES (?, NULL)');
+              stmt.run(req.body.players[k].name);
+            }
+            k++;
+          });
+          
+          console.log('inserting into played_in');
+          stmt = db.prepare('INSERT INTO played_in VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
+          stmt.run(
+            req.body.players[i].name,
+            curGameID,
+            req.body.players[i].character,
+            req.body.players[i].class,
+            req.body.players[i].kills,
+            req.body.players[i].specials,
+            req.body.players[i].ranged,
+            req.body.players[i].melee,
+            req.body.players[i].damageDealt,
+            req.body.players[i].damageMonsters,
+            req.body.players[i].damageTaken,
+            req.body.players[i].hs,
+            req.body.players[i].saves,
+            req.body.players[i].revives,
+            req.body.players[i].ff
+          );
+        });
+      };
+      stmt.finalize();
       
-      console.log("inserting into played_in")
-      db.run(
-        'INSERT INTO played_in VALUES ($name, $curGameID, $character, $class, $kills, $specials, $ranged, $melee, $damageDealt, $damageMonsters, $damageTaken, $hs, $saves, $revives, $ff)',
-        {
-          $name: req.body.players[i].name,
-          $curGameID: curGameID,
-          $character: req.body.players[i].character,
-          $class: req.body.players[i].class,
-          $kills: req.body.players[i].kills,
-          $specials: req.body.players[i].specials,
-          $ranged: req.body.players[i].ranged,
-          $melee: req.body.players[i].melee,
-          $damageDealt: req.body.players[i].damageDealt,
-          $damageMonsters: req.body.players[i].damageMonsters,
-          $damageTaken: req.body.players[i].damageTaken,
-          $hs: req.body.players[i].hs,
-          $saves: req.body.players[i].saves,
-          $revives: req.body.players[i].revives,
-          $ff: req.body.players[i].ff,
-        },
-        function(err) {
-          if (err) {
-            console.log('Error in app.post(/addGame/) on INSERTING played_in ON: ' + i);
-            console.log('    error: ' + err);
-            
-            if (i === 3) res.send({ status: false, message: 'POST ERROR' });
-          } 
-          else {
-            if (i === 3) res.send({ status: true, message: 'POST SUCCESS' });
-          }
-        }
-      );
-    };
+      res.send({ status: true, message: 'POST SUCCESS' });
+    });
   });
 });
 
